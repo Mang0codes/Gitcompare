b6ac9d9cfbee6826d45443475ea1fbff49dc78c0
Albia Hossain <albiahossain0@gmail.com>
2025-09-10 01:07:20 +0530

    metadata

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Footer from '@/Components/Footer';
 import Landing from '@/Components/Landing';
